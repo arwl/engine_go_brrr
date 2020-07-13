ea33399d69283a59f0d488ecec91ecc5d30b3f6c
Tom Barwell <tomb@rwell.co.uk>
2020-07-13 23:26:41 +0100

    Added code to load opengl shaders from file into a char*

---
+++ egb_shader/load_shader.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "load_shader.h"
+
+int load_shader(char* filename)
+{
+    char* buffer;
+    unsigned long length;
+    FILE* f = fopen(filename, "rb");
+
+    if (f) {
+        fseek(f, 0, SEEK_END);
+        length = ftell(f);
+        fseek(f, 0, SEEK_SET);
+        buffer = malloc(length + 1);
+        if (buffer) {
+            fread(buffer, 1, length, f);
+        }
+        buffer[length] = '\0'; // Add string terminator
+    } else {
+        fprintf(stderr, "Could not load shader file at %s\n", filename);
+    }
+
+    if (buffer) {
+        printf("%s", buffer);
+    }
+
+    return 0;
+}
