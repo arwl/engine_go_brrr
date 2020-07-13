aa3886da6febe36160a4e4a9309a196ce1c03ea1
Tom Barwell <tomb@rwell.co.uk>
2020-07-13 23:26:59 +0100

    Cleaned up build script slightly

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 
 packages="glfw3 glew"
 bin_name="main"
+compile_flags=$(pkg-config --cflags $packages)
+lib_flags=$(pkg-config --libs $packages)
 
-cc $(pkg-config --cflags $packages) -o $bin_name main.c $(pkg-config --libs $packages)
+cc $compile_flags -o $bin_name main.c $lib_flags
